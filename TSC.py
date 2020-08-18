2df56369de1c369a5a26e69b166e50e6de73b76c
Geraldine Zanolli <zanolli.geraldine@gmail.com>
2020-08-18 09:32:02 -0700

    Add Flask

--- TSC.py
+++ TSC.py
@@ -19,6 +19,8 @@ import os
 import sys
 import logging
 
+
+
 # Suppress InsecureRequestWarning
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -30,9 +32,17 @@ try:
         TABLEAU_SITE_NAME = os.environ.get('TABLEAU_SITE_NAME')
         TABLEAU_TOKEN_NAME = os.environ.get('TABLEAU_TOKEN_NAME')
         TABLEAU_EVENT_NAME = os.environ.get('TABLEAU_EVENT_NAME')
+        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
 except:
         MESSAGE = 'Manually set the Message if youre not running through heroku or have not set vars in ENV'
         TOKEN = 'Manually set the API Token if youre not running through heroku or have not set vars in ENV'
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def respond():
+    print(request.json);
+    return Response(status=200)
 
 def main():
 
@@ -51,7 +61,7 @@ def main():
 
                 new_webhook = TSC.WebhookItem()
                 new_webhook.name = "Webooks created from Heroku"
-                new_webhook.url = "https://webhook.site/c0958e0a-ad1d-493b-97bb-7344586684a5"
+                new_webhook.url = "https://"+HEROKU_APP_NAME+".herokuapp.com/"
                 new_webhook.event = TABLEAU_EVENT_NAME
                 print(new_webhook)
                 new_webhook = server.webhooks.create(new_webhook)
