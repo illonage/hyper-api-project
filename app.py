42716ecde5837f4ea13d9b9c9453b67b0a980c34
Geraldine Zanolli <zanolli.geraldine@gmail.com>
2021-04-30 11:36:21 -0700

    udate app

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import json
 import os
 import re
 import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from tableauhyperapi import Connection, HyperProcess, SqlType, TableDefinition, \
     escape_string_literal, escape_name, NOT_NULLABLE, Telemetry, Inserter, CreateMode, TableName
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from flask import Flask, request, render_template, redirect, url_for, flash
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -17,7 +18,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 @app.route('/create', methods=['POST'])
 def create():
     with HyperProcess(Telemetry.SEND_USAGE_DATA_TO_TABLEAU) as hyper:
-    print("The HyperProcess has started.")
+        print("The HyperProcess has started.")
 
     with Connection(hyper.endpoint, 'TrivialExample.hyper', CreateMode.CREATE_AND_REPLACE) as connection:
         print("The connection to the Hyper file is open.")
@@ -25,7 +26,7 @@ def create():
         example_table = TableDefinition(TableName('Extract','Extract'), [
             TableDefinition.Column('rowID', SqlType.big_int()),
             TableDefinition.Column('value', SqlType.big_int()),
-         ])
+        ])
         print("The table is defined.")
         connection.catalog.create_table(example_table)
         with Inserter(connection, example_table) as inserter:
