0ed7d5e4ea0229ea8a691fc434eb45c8a8c19117
Geraldine Zanolli <zanolli.geraldine@gmail.com>
2021-05-03 19:25:19 -0700

    add path to database

--- app.py
+++ app.py
@@ -58,11 +58,8 @@ def create():
                     object_name = file_name
                 s3_client = boto3.client('s3', aws_access_key_id=os.environ.get('aws_access_key_id'), 
                             aws_secret_access_key= os.environ.get('aws_secret_access_key'))
-                s3 = boto3.resource('s3')
-                print(path_to_database)
-                print(type(path_to_database))
                 try:
-                    s3.meta.client.upload_fileobj(path_to_database, file_name, 'hyper.hyper')
+                    response = s3_client.upload_file(String(path_to_database),file_name, object_name)
                 except ClientError as e:
                     logging.error(e)
                     return False
